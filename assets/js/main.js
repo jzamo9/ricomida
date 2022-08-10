98fb32322224bde099e82a9a548d3139f17d5f84
Javier Zamorano <zamorano.javier.m@gmail.com>
2022-08-10 11:01:29 -0400

    Desafio ricomida final

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+
+$(document).ready (function (){
+
+    // $("#button").css("color", "red")
+
+    // $("#button").dblclick(function (){
+    //     $(this).hide();
+    // });
+
+    $("#titulo-1").dblclick(function(){
+        $(this).css("color", "red");
+    })
+
+    $("#titulo-2").dblclick(function(){
+        $(this).css("color", "red");
+    })
+
+    $('.button-1').click(function(){
+        $(this).on(alert("El correo fue enviado correctamente"));
+ })
+
+ $('.button-1-m').click(function(){
+    $(this).on(alert("El correo fue enviado correctamente"));
+})
+        
+    $('.button-1').tooltip();
+    $('.button-2').tooltip();
+    
+
+    $(".card").click(function(){
+        $("#cards").toggle();
+     })
+
+
+     $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        // nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+ 
+
+
+
+   
+     
+
+
+});
+
